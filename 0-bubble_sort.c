a99984d69ddd7c519ae12f123695ab49020234ec
Kamalusta <kamalusta@gmail.com>
2024-01-17 12:50:30 -0800

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorting function
+ * @array: - given array
+ * @size: - size of array
+ *
+ * Return: Always 0
+ */
+void bubble_sort(int *array, size_t size)
+{
+  size_t i, j, buff;
+
+  for (j = 1; j < size ; j++)
+    {
+      for (i = 0; i < size - j; i++)
+	{
+	  if (array[i] > array[i + 1])
+	    {
+	      buff = array[i];
+	      array[i] = array[i + 1];
+	      array[i + 1] = buff;
+	      print_array(array, size);
+	    }
+	}
+    }
+}
