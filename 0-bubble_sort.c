b970be1e50d5f6db33024950cf29eccd6f317f17
Kamalusta <kamalusta@gmail.com>
2024-01-17 13:24:52 -0800

    betty stile

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,19 +9,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-  size_t i, j, buff;
+size_t i, j, buff;
 
-  for (j = 1; j < size ; j++)
-    {
-      for (i = 0; i < size - j; i++)
-	{
-	  if (array[i] > array[i + 1])
-	    {
-	      buff = array[i];
-	      array[i] = array[i + 1];
-	      array[i + 1] = buff;
-	      print_array(array, size);
-	    }
-	}
-    }
+for (j = 1; j < size; j++)
+{
+for (i = 0; i < size - j; i++)
+{
+if (array[i] > array[i + 1])
+{
+buff = array[i];
+array[i] = array[i + 1];
+array[i + 1] = buff;
+print_array(array, size);
+}
+}
+}
 }
